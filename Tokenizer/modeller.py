690f32b3c5e616f7002baf4320a47ed7843a85b1
zubairabid <zubairabid1999+github@gmail.com>
2019-09-25 12:09:02 +0530

    some stuff

--- Tokenizer/modeller.py
+++ Tokenizer/modeller.py
@@ -1,12 +1,16 @@
-from nltk.tokenize import word_tokenize
 import pickle
+from collections import Counter, defaultdict
+
+def reg_tokenize(sent):
+    return re.findall(r'[A-Z0-9a-z]+[A-Z0-9a-z._%+-]*@[A-Z0-9a-z]+(?:\.[A-Z0-9a-z]+)+|(?<= )[$€£¥₹]?[0-9]+(?:[,.][0-9]+)*[$€£¥₹]?|(?:(?:https?:\/\/(?:www.)?)|www.)[A-Z0-9a-z_-]+(?:\.[A-Z0-9a-z_\/-]+)+|(?<= )@[A-Z0-9a-z._%+]+|#[A-Za-z0-9]+(?:\.[A-Za-z0-9]+)*|\.{3,}|[!"#$%&\'()*+,\-./:;<=>?@\[\\\]\^_`{\|}~]|\w+', sent)
 
 def ngrams(sentence, n):
     '''
     This currently does not accomodate for numbers with punctuation.
     '''
+    print("Getting ngrams for n = {0}, sentence = {1}".format(n, sentence))
     sentence = sentence.lower()
-    tokens = word_tokenize(sentence)
+    tokens = reg_tokenize(sentence)
     ngrams = zip(*[tokens[i:] for i in range(n)])
     return [ngram for ngram in ngrams]
 
@@ -25,13 +29,14 @@ def create_model(file="corpus2.txt", n=3):
                 waft = ngram[n-1]
                 model[wprec][waft] += 1
         f.close()    
-
+    print("counted")
     for wprec in model:
         total_count = float(sum(model[wprec].values()))
         for waft in model[wprec]:
             model[wprec][waft] /= total_count
             
     return model
+    print("probabilitied")
 
 model = create_model(n=3)
 with open('filename.pickle', 'wb') as handle:
